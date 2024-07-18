bd6c83e711e552654325c713c816c2030791b2ec
tadester <obasantade@gmail.com>
2024-07-17 23:22:34 -0600

    restarting

---
+++ client/src/api.js
@@ -0,0 +1,9 @@
+// client/src/api.js
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://localhost:5000/api',
+});
+
+export const fetchUserData = () => API.get('/user/data');
+export const unfollowUser = (userId) => API.post(`/user/unfollow/${userId}`);
